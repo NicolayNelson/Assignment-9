551f11430067fe08040c3a9a8c59c32f386d2930
unknown <MyCraftablRomnce@gmail.com>
2020-03-11 22:39:54 -0700

    Answered question 2

--- script.js
+++ script.js
@@ -4,14 +4,13 @@
 console.log(
   "What worked well for you over the last 9 weeks that aided in your learning process?"
 );
-console.log("Going to class and getting help instead of just looking things up online helped 
- me a lot more. The stuff online was confusing and people ");
+console.log("Going to class and getting help instead of just looking things up online helped me a lot more. The stuff online was confusing and people would contradict each other");
 
 // 2. Write a console.log() statement that answers the following question: What would you
 // change about the course?
 
 console.log("What would you change about the course?");
-console.log("");
+console.log("I would make it a full semester because its not enough time to learn it all.");
 
 // 3. Write a console.log() statement that answers the following question: Did you learn
 // what you expected to learn in this course?
